b6a0db68030b59c4928647b4041823304d8e6cbb
Nathaniel <nathaniel.lehman.v1.0@gmail.com>
2025-07-28 16:35:33 -0400

    p3 - split settings into dynamic and static. added increase difficulty

--- settings.py
+++ settings.py
@@ -14,27 +14,22 @@ class Settings:
         self.screen_h = 800
         self.FPS = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'new background.jpg'
+        self.difficulty_scale = 1.1
 
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'new ship.png'
         self.ship_w = 40
         self.ship_h = 60
-        self.ship_speed = 5
-        self.starting_ship_count = 3
 
         self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'new beam2.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'new laser.mp3'
         self.impact_sound = Path.cwd() / 'Assets' / 'sound' / 'rumble.flac'
-        self.bullet_speed = 7
-        self.bullet_w = 25
-        self.bullet_h = 80
-        self.bullet_amount = 5
+      
 
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'alien.png'
         self.alien_w = 40
         self.alien_h = 40
-        self.fleet_speed = 3
         self.fleet_direction = 1
-        self.fleet_drop_speed = 8
+
 
         self.button_w = 200
         self.button_h = 50
@@ -44,6 +39,21 @@ class Settings:
         self.button_font_size = 48
         self.HUD_font_size= 20
         self.font_file = Path.cwd() / 'Assets' / 'Fonts' / 'Silkscreen' / 'Goldman-Bold.ttf'
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 5
+        self.starting_ship_count = 3
+        self.bullet_w = 25
+        self.bullet_h = 80
+        self.bullet_speed = 7
+        self.bullet_amount = 5
+        self.fleet_speed = 3
+        self.fleet_drop_speed = 8
+
+    def increase_difficulty(self):
+        self.ship_speed *= self.difficulty_scale
+        self.bullet_speed *= self.difficulty_scale
+        self.fleet_speed *= self.difficulty_scale
         
 
 
